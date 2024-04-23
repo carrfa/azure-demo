3dafe1b8709cb7c54cd9d909feb2173209c3802c
CISEL <53046543+carrfa@users.noreply.github.com>
2024-04-23 08:04:22 +0200

    vm bad example

--- main.tf
+++ main.tf
@@ -10,3 +10,31 @@ resource "azurerm_resource_group" "rg-aks-1" {
   name     = var.resource_group_name_2
   location = var.location
 }
+resource "azurerm_linux_virtual_machine" "bad_linux_example" {
+  name                            = "bad-linux-machine"
+  resource_group_name             = azurerm_resource_group.rg-aks-1.name
+  location                        = azurerm_resource_group.rg-aks-1.location
+  size                            = "Standard_F2"
+  admin_username                  = "adminuser"
+  admin_password                  = "somePassword"
+  disable_password_authentication = false
+}
+
+resource "azurerm_virtual_machine" "bad_example" {
+    name                            = "bad-linux-machine"
+    resource_group_name             = azurerm_resource_group.rg-aks-1.name
+    location                        = azurerm_resource_group.rg-aks-1.location
+    size                            = "Standard_F2"
+    admin_username                  = "adminuser"
+    admin_password                  = "somePassword"
+
+    os_profile {
+        computer_name  = "hostname"
+        admin_username = "testadmin"
+        admin_password = "Password1234!"
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = false
+    }
+  }
