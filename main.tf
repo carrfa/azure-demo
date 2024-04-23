b5e67966348f3600085cac721c80fb8496148deb
CISEL <53046543+carrfa@users.noreply.github.com>
2024-04-23 09:17:53 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,12 +12,30 @@ resource "azurerm_resource_group" "rg-aks-1" {
 }
 resource "azurerm_linux_virtual_machine" "bad_linux_example" {
   name                            = "bad-linux-machine"
-  resource_group_name             = azurerm_resource_group.rg-aks-2.name
-  location                        = azurerm_resource_group.rg-aks-2.location
+  resource_group_name             = azurerm_resource_group.rg-aks-1.name
+  location                        = azurerm_resource_group.rg-aks-1.location
   size                            = "Standard_F2"
   admin_username                  = "adminuser"
   admin_password                  = "somePassword"
   disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.example2.id,
+  ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+}
+resource "azurerm_network_interface" "example2" {
+  name                = "example2-nic"
+  location            = azurerm_resource_group.rg-aks-1.location
+  resource_group_name = azurerm_resource_group.rg-aks-1.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 resource "azurerm_virtual_machine" "vm" {
   name                  = "my-vm"
