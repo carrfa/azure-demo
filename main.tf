7d061446498adad33abc29d6299cba95adea029e
CISEL <53046543+carrfa@users.noreply.github.com>
2024-04-23 08:45:17 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,31 +10,60 @@ resource "azurerm_resource_group" "rg-aks-1" {
   name     = var.resource_group_name_2
   location = var.location
 }
-resource "azurerm_linux_virtual_machine" "bad_linux_example" {
-  name                            = "bad-linux-machine"
-  resource_group_name             = azurerm_resource_group.rg-aks-1.name
-  location                        = azurerm_resource_group.rg-aks-1.location
-  size                            = "Standard_F2"
-  admin_username                  = "adminuser"
-  admin_password                  = "somePassword"
-  disable_password_authentication = false
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "my-vm"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg-aks-1.name
+  vm_size               = "Standard_DS2_v2"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "my-vm-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "my-vm"
+    admin_username = "adminuser"
+    admin_password = "Password1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
 }
 
-resource "azurerm_virtual_machine" "bad_example" {
-    name                            = "bad-linux-machine"
-    resource_group_name             = azurerm_resource_group.rg-aks-1.name
-    location                        = azurerm_resource_group.rg-aks-1.location
-    size                            = "Standard_F2"
-    admin_username                  = "adminuser"
-    admin_password                  = "somePassword"
+resource "azurerm_network_interface" "nic" {
+  name                = "my-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg-aks-1.name
 
-    os_profile {
-        computer_name  = "hostname"
-        admin_username = "testadmin"
-        admin_password = "Password1234!"
-    }
-
-    os_profile_linux_config {
-        disable_password_authentication = false
-    }
+  ip_configuration {
+    name                          = "my-nic-ipconfig"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
   }
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "my-subnet"
+  resource_group_name  = azurerm_resource_group.rg-aks-1.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes      = ["10.0.1.0/24"]
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "my-vnet"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg-aks-1.name
+  address_space       = ["10.0.0.0/16"]
+}
