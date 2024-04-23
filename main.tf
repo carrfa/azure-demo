4467e0473bfbe37d7ee5055bf090cfa5ee235a89
CISEL <53046543+carrfa@users.noreply.github.com>
2024-04-23 09:13:34 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,15 @@ resource "azurerm_resource_group" "rg-aks-1" {
   name     = var.resource_group_name_2
   location = var.location
 }
+resource "azurerm_linux_virtual_machine" "bad_linux_example" {
+  name                            = "bad-linux-machine"
+  resource_group_name             = azurerm_resource_group.example.name
+  location                        = azurerm_resource_group.example.location
+  size                            = "Standard_F2"
+  admin_username                  = "adminuser"
+  admin_password                  = "somePassword"
+  disable_password_authentication = false
+}
 resource "azurerm_virtual_machine" "vm" {
   name                  = "my-vm"
   location              = var.location
